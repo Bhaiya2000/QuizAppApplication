b0650fcd3c8a111bb1569beae6dceae81bf0181f
anandkushuklasatna_3406a40e433f <anandku.shuklasatna@gmail.com>
2023-07-30 02:02:34 +0530

    first chalenge

---
+++ src/main/java/com/anand/quizapp/Dao/QuizDao.java
@@ -0,0 +1,9 @@
+package com.anand.quizapp.Dao;
+
+import com.anand.quizapp.models.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+}
